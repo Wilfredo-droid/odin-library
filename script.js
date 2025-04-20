b584f0de4299e494aaa75b9c4169b30d3dc6b682
Wilfredo-droid <wilfredoandres10@gmail.com>
2025-04-20 13:10:13 -0400

    Make function to cycle through book array

--- script.js
+++ script.js
@@ -9,7 +9,23 @@ function bookConstructor(title, author, pages, read) {
 };
 
 
+let cycleThroughLibrary = () => {
+
+    for (let i = 0; i < myBookLibrary.length; i++) {
+        let book = myBookLibrary[i];
+        console.log(book.title);
+    }
+
+}
+
+
+
 let book1 = new bookConstructor("The Hobbit", "J.R.R. Tolkien", 295, true);
+let book2 = new bookConstructor("The Lord of the Rings", "J.R.R. Tolkien", 1178, true);
+
 
 myBookLibrary.push(book1);
+myBookLibrary.push(book2);
+
+cycleThroughLibrary();
 
